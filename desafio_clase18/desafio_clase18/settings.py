0ad3f406b50bd51bf9a2482f66980dca3bf9f9be
lautarofasc <lautaro9510@gmail.com>
2022-12-27 23:23:17 -0300

    Se modifica el cargador de templates y el template

--- desafio_clase18/desafio_clase18/settings.py
+++ desafio_clase18/desafio_clase18/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'familia.apps.FamiliaConfig',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'desafio_clase18.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
