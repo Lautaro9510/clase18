8fe02ee44408c7ec970cc1e286a3ef33aac0cd4f
lautarofasc <lautaro9510@gmail.com>
2022-12-27 22:00:19 -0300

    Se agregan modelos para agregar familiares/templat

---
+++ desafio_clase18/familia/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from familia.models import Familia
+
+# Create your views here.
+
+def crear_familiar(request):
+    nuevo_familiar = Familia.objects.create(nombre= 'Hector',anio_nacimiento= 1963, casado= True)
+    print(nuevo_familiar)
+    return HttpResponse('Un miembro de la familia se ha añadido')
+
+def lista_familia(request):
+    familia_entera = Familia.objects.all()
+    print(familia_entera)
+    context = {
+        'Familia':familia_entera,
+    }
+    return render(request, 'familia.html', context=context)
